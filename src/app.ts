4c19401c53e9551baeb2f51b045cacbb96d80cc0
idrissa wade <derisswade@gmail.com>
2025-07-13 15:04:09 +0000

    fix: mise à jour de l'origine CORS pour le déploiement en production

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ import statsRoutes from './routes/statistique.routes';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://e-learning-frontend-jet.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization','access-control-allow-origin'],
